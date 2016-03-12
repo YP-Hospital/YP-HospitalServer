988fdea72a3274fd84b9e54daa7ea203f683620c
MaryAlex <worker96@mail.ru>
2016-03-13 02:15:55 +0300

    version 2.0

--- src/com/company/TCPServer.java
+++ src/com/company/TCPServer.java
@@ -7,15 +7,17 @@ import java.net.Socket;
 public class TCPServer extends Thread {
 
     public static final int SERVER_PORT = 8080;
+    public static final String STOP_WORDS = "This is a stop message";
     private boolean running = false;
     private DataOutputStream outputStream;
     private DataInputStream inputStream;
+    private String message = "";
 
     /**
      * Method to send the messages from server to client
      * @param message the message sent by the server
      */
-    public void sendMessage (String message) {
+    public void sendMessage(String message) {
         try {
             if (outputStream != null) {
                 outputStream.writeUTF(message);
@@ -29,38 +31,61 @@ public class TCPServer extends Thread {
     @Override
     public void run() {
         super.run();
-
         running = true;
-
+        File dataBase = new File("hospital.db");
         try {
             System.out.println("S: Connecting...");
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-            System.out.println("Waiting for a client...");
-            Socket client = serverSocket.accept();
-            System.out.println("S: Receiving...");
-            InputStream sin = client.getInputStream();
-            OutputStream sout = client.getOutputStream();
-
-            inputStream = new DataInputStream(sin);
-            outputStream = new DataOutputStream(sout);
             try {
                 while (running) {
-                    String message = inputStream.readUTF();
-                    System.out.println("Message from client: " + message);
+                    clientRequestHandling(dataBase, serverSocket);
                 }
-
             } catch (Exception e) {
                 System.out.println("S: Error");
                 e.printStackTrace();
-            } finally {
-                client.close();
-                System.out.println("S: Done.");
             }
-
         } catch (Exception e) {
             System.out.println("S: Error");
             e.printStackTrace();
         }
+    }
+
+    private void clientRequestHandling(File dataBase, ServerSocket serverSocket) throws IOException {
+        Socket client;
+        client = getClient(serverSocket);
+        getMessages();
+        updateDataBaseOnClient(dataBase, client);
+        client.close();
+    }
+
+    private void updateDataBaseOnClient(File dataBase, Socket client) throws IOException {
+        byte[] fileInBytes = new byte[(int) dataBase.length()];
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(dataBase));
+        bis.read(fileInBytes, 0, fileInBytes.length);
+        OutputStream os = client.getOutputStream();
+        os.write(fileInBytes, 0, fileInBytes.length);
+        os.flush();
+        os.close();
+        bis.close();
+    }
+
+    private void getMessages() throws IOException {
+        while (!message.equals(STOP_WORDS)) {
+            message = inputStream.readUTF();
+            System.out.println("Message from client: " + message);
+        }
+        message = "";
+    }
+
+    private Socket getClient(ServerSocket serverSocket) throws IOException {
+        System.out.println("Waiting for a client...");
+        Socket client = serverSocket.accept();
+        System.out.println("S: Receiving...");
+        InputStream sin = client.getInputStream();
+        OutputStream sout = client.getOutputStream();
+        inputStream = new DataInputStream(sin);
+        outputStream = new DataOutputStream(sout);
+        return client;
     }
 
     public void stopServer() {
